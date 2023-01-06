6cd77ecf680a4e59dce120e8e897d293e1c67f80
atgctg <105969161+atgctg@users.noreply.github.com>
2023-01-06 18:30:49 +0100

    update icon on tab changes

--- background.js
+++ background.js
@@ -1,23 +1,47 @@
-chrome.action.onClicked.addListener((tab) => {
-  chrome.storage.local.get('darkModeEnabled').then((data) => {
-    if (!data.darkModeEnabled) {
-      chrome.scripting.insertCSS({
-        target: { tabId: tab.id },
-        files: ['dark-mode.css'],
-      })
-      chrome.action.setIcon({
-        path: 'icons/on.png',
-      })
-      chrome.storage.local.set({ darkModeEnabled: true })
-    } else {
-      chrome.scripting.removeCSS({
-        target: { tabId: tab.id },
-        files: ['dark-mode.css'],
-      })
-      chrome.action.setIcon({
-        path: 'icons/off.png',
-      })
-      chrome.storage.local.set({ darkModeEnabled: false })
-    }
+async function getForcedDarkModeTabIds() {
+  let { forcedDarkModeTabIds } = await chrome.storage.sync.get(
+    'forcedDarkModeTabIds'
+  )
+  if (!forcedDarkModeTabIds) forcedDarkModeTabIds = []
+  return forcedDarkModeTabIds
+}
+
+function setForcedDarkModeTabIds(forcedDarkModeTabIds) {
+  chrome.storage.sync.set({ forcedDarkModeTabIds })
+}
+
+function setIcon(on = true) {
+  chrome.action.setIcon({
+    path: on ? 'icons/on.png' : 'icons/off.png',
   })
+}
+
+chrome.action.onClicked.addListener(async (tab) => {
+  const forcedDarkModeTabIds = await getForcedDarkModeTabIds()
+  if (!forcedDarkModeTabIds.includes(tab.id)) {
+    chrome.scripting.insertCSS({
+      target: { tabId: tab.id },
+      files: ['dark-mode.css'],
+    })
+
+    setForcedDarkModeTabIds([...forcedDarkModeTabIds, tab.id])
+    setIcon(true)
+  } else {
+    chrome.scripting.removeCSS({
+      target: { tabId: tab.id },
+      files: ['dark-mode.css'],
+    })
+
+    setForcedDarkModeTabIds(forcedDarkModeTabIds.filter((id) => id !== tab.id))
+    setIcon(false)
+  }
+})
+
+chrome.tabs.onActivated.addListener(async ({ tabId }) => {
+  const forcedDarkModeTabIds = await getForcedDarkModeTabIds()
+  if (forcedDarkModeTabIds.includes(tabId)) {
+    setIcon(true)
+  } else {
+    setIcon(false)
+  }
 })
